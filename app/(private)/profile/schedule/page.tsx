b31944c20b32ceb2cb8966dcb792475ea2434641
honghaine <111057775+hai-to-CLV@users.noreply.github.com>
2023-12-16 18:30:25 +0700

    Schedule

---
+++ app/(private)/profile/schedule/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/utils/navbar'
+import React from 'react'
+
+const Schedule = () => {
+  return (
+    <>
+        <Navbar></Navbar>
+
+    </>
+  )
+}
+
+export default Schedule
